f335de08548e09384a8fcb7c8d56eea2e87687c2
João <Sem e-mail>
2026-06-01 14:51:39 -0300

    Cria versão inicial da tela de cadastro de cliente

---
+++ src/main/java/com/genetico/views/CadastroCliente.java
@@ -0,0 +1,29 @@
+package com.genetico.views;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route("/cadastrar-cliente")
+public class CadastroCliente extends VerticalLayout {
+
+    public CadastroCliente() {
+        var titulo = new H3("Cadastro de cliente");
+        var formularioCadastro = new FormLayout();
+        var descricao = new TextField("Descrição");
+        var latitude = new TextField("Latitude");
+        var longitude = new TextField("Longitude");
+
+        formularioCadastro.add(descricao, latitude, longitude);
+
+        var botaoCadastrarCliente = new Button("Cadastrar cliente");
+        botaoCadastrarCliente.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        add(titulo, formularioCadastro, botaoCadastrarCliente);
+    }
+
+}
