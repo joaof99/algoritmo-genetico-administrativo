80146adc8344432a593c21e0f0bfb52e61f1903a
João <Sem e-mail>
2026-06-02 23:17:19 -0300

    Cria cliente

--- src/main/java/com/genetico/model/Cliente.java
+++ src/main/java/com/genetico/model/Cliente.java
@@ -1,32 +1,54 @@
 package com.genetico.model;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public class Cliente {
-    @NotNull(message = "Descrição é obrigatório")
-    private final String descricao;
+    @NotEmpty(message = "Descrição não pode ser vazia")
+    private String descricao;
 
-    @NotNull(message = "Latitude é obrigatório")
-    private final String latitude;
+    @NotNull(message = "Latitude é obrigatória")
+    @DecimalMin(value = "-90.0")
+    @DecimalMax(value = "90.0")
+    private Double latitude;
 
-    @NotNull(message = "Longitude é obrigatório")
-    private final String longitude;
+    @NotNull(message = "Longitude é obrigatória")
+    @DecimalMin(value = "-180.0")
+    @DecimalMax(value = "180.0")
+    private Double longitude;
 
-    public Cliente(String descricao, String latitude, String longitude) {
+    public Cliente() {
+
+    }
+
+    public Cliente(String descricao, Double latitude, Double longitude) {
         this.descricao = descricao;
         this.latitude = latitude;
         this.longitude = longitude;
     }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
     }
 }
