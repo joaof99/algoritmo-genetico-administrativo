f0aad250486d1d454086ac9448407a20bd8f541a
João <Sem e-mail>
2026-06-02 23:17:13 -0300

    Melhora view de cadastro cliente

--- src/main/java/com/genetico/views/CadastroClienteView.java
+++ src/main/java/com/genetico/views/CadastroClienteView.java
@@ -1,29 +1,43 @@
 package com.genetico.views;
 
+import com.genetico.model.Cliente;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route("/cadastrar-cliente")
+@PageTitle("Cadastro de cliente")
 public class CadastroClienteView extends VerticalLayout {
+    private final BeanValidationBinder<Cliente> binder = new BeanValidationBinder<>(Cliente.class);
+    private final TextField descricao = new TextField("Digite a descrição");
+    private final TextField latitude = new TextField("Digite a latitude");
+    private final TextField longitude = new TextField("Digite a longitude");
+    private final Cliente cliente = new Cliente();
 
     public CadastroClienteView() {
-        var titulo = new H3("Cadastro de cliente");
+        add(descricao, latitude, longitude);
+
+        binder.bindInstanceFields(this);
+        binder.setBean(cliente);
+
         var formularioCadastro = new FormLayout();
-        var descricao = new TextField("Descrição");
-        var latitude = new TextField("Latitude");
-        var longitude = new TextField("Longitude");
+        var titulo = new H3("Cadastro de cliente");
 
         formularioCadastro.add(descricao, latitude, longitude);
 
         var botaoCadastrarCliente = new Button("Cadastrar cliente");
         botaoCadastrarCliente.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
+        botaoCadastrarCliente.addClickListener(clickBotao -> {
+
+        });
+
         add(titulo, formularioCadastro, botaoCadastrarCliente);
     }
-
 }
