e551ce40749f16304b13d9244f786d0c442f635b
João <Sem e-mail>
2026-06-04 19:49:25 -0300

    Altera mapeamento de cliente para suportar Hibernate

--- src/main/java/com/genetico/model/Cliente.java
+++ src/main/java/com/genetico/model/Cliente.java
@@ -1,33 +1,38 @@
 package com.genetico.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     @NotEmpty(message = "Descrição não pode ser vazia")
     private String descricao;
 
     @NotNull(message = "Latitude é obrigatória")
-    @DecimalMin(value = "-90.0")
-    @DecimalMax(value = "90.0")
+    @DecimalMin(value = "-90.0", message = "Latitude mínima deve ser -90.0")
+    @DecimalMax(value = "90.0", message = "Latitude máxima deve ser 90.0")
     private Double latitude;
 
     @NotNull(message = "Longitude é obrigatória")
-    @DecimalMin(value = "-180.0")
-    @DecimalMax(value = "180.0")
+    @DecimalMin(value = "-180.0", message = "Longitude mínima deve ser -180.0")
+    @DecimalMax(value = "180.0", message = "Longitude máxima deve ser 180.0")
     private Double longitude;
 
     public Cliente() {
 
     }
 
-    public Cliente(String descricao, Double latitude, Double longitude) {
-        this.descricao = descricao;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
