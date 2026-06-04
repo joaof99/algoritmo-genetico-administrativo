ae1504eefaa5b1d4a20484e85db007aae04495ee
João <Sem e-mail>
2026-06-04 19:48:50 -0300

    Atualiza view para persistir cliente

--- src/main/java/com/genetico/views/CadastroClienteView.java
+++ src/main/java/com/genetico/views/CadastroClienteView.java
@@ -1,15 +1,18 @@
 package com.genetico.views;
 
 import com.genetico.model.Cliente;
+import com.genetico.repository.ClienteRepository;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("/cadastrar-cliente")
 @PageTitle("Cadastro de cliente")
@@ -19,6 +22,9 @@ public class CadastroClienteView extends VerticalLayout {
     private final TextField latitude = new TextField("Digite a latitude");
     private final TextField longitude = new TextField("Digite a longitude");
     private final Cliente cliente = new Cliente();
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     public CadastroClienteView() {
         add(descricao, latitude, longitude);
@@ -35,7 +41,10 @@ public class CadastroClienteView extends VerticalLayout {
         botaoCadastrarCliente.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         botaoCadastrarCliente.addClickListener(clickBotao -> {
-
+            if (!binder.validate().hasErrors()) {
+                clienteRepository.save(cliente);
+                Notification.show("Cliente salvo com sucesso");
+            }
         });
 
         add(titulo, formularioCadastro, botaoCadastrarCliente);
