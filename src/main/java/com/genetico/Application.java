c196ef65df63b2a2e1a857ebfad306bd88ed177e
João <Sem e-mail>
2026-06-06 15:32:50 -0300

    Ajusta compatibilidades no pom para suportar flyway e remove import e anotação não mais utilizada

--- src/main/java/com/genetico/Application.java
+++ src/main/java/com/genetico/Application.java
@@ -1,6 +1,5 @@
 package com.genetico;
 
-import com.vaadin.flow.theme.aura.Aura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-@StyleSheet(Aura.STYLESHEET)
 @StyleSheet("styles.css") // Your custom styles
 public class Application implements AppShellConfigurator {
 
