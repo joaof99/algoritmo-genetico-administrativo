e802352c906bf258fc9d902c980a8f3c1e50d1d7
João <Sem e-mail>
2026-06-08 21:48:32 -0300

    Adiciona testes novos

--- src/test/java/com/genetico/views/MainViewTest.java
+++ src/test/java/com/genetico/views/MainViewTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -21,6 +20,7 @@ import java.util.ArrayList;
 import static com.github.mvysny.kaributesting.v10.GridKt._size;
 import static com.github.mvysny.kaributesting.v10.LocatorJ.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -58,6 +58,23 @@ class MainViewTest {
     }
 
     @Test
+    @DisplayName("Deve limpar o formulário após salvar um cliente")
+    void deveLimparFormularioCorretamenteAposSalvarCliente() {
+        when(clienteService.salvar(any())).thenReturn(new Cliente());
+
+        _setValue(_get(TextField.class, spec -> spec.withLabel("Digite a descrição")), "Cliente Teste");
+        _setValue(_get(TextField.class, spec -> spec.withLabel("Digite a latitude")), "-23.5");
+        _setValue(_get(TextField.class, spec -> spec.withLabel("Digite a longitude")), "-46.6");
+        _click(_get(Button.class, spec -> spec.withText("Cadastrar cliente")));
+
+        var todosInputsEstaoLimpos = _find(TextField.class)
+                .stream()
+                .allMatch(field -> field.getValue().isEmpty());
+
+        assertTrue(todosInputsEstaoLimpos);
+    }
+
+    @Test
     @DisplayName("Não deve atualizar o grid se ocorrer exceção ao salvar o cliente")
     void naoDeveAtualizarGridSeOcorrerExcecaoAoSalvarCliente() {
         when(clienteService.salvar(any())).thenThrow(new RuntimeException("Erro no banco"));
@@ -67,7 +84,14 @@ class MainViewTest {
         _setValue(_get(TextField.class, spec -> spec.withLabel("Digite a longitude")), "-46.6");
 
         _click(_get(Button.class, spec -> spec.withText("Cadastrar cliente")));
+        var quantidadeItensGrid = _size(_get(Grid.class, spec -> spec.withId("grid-clientes")));
+        assertEquals(0, quantidadeItensGrid);
+    }
 
-        assertEquals(0, _size(_get(Grid.class)));
+    @Test
+    @DisplayName("Não deve salvar se o formulário estiver inválido")
+    void naoDeveSalvarComFormularioInvalido() {
+        _click(_get(Button.class, spec -> spec.withText("Cadastrar cliente")));
+        verify(clienteService, never()).salvar(any());
     }
 }
