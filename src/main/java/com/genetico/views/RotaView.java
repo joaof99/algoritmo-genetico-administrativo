bd4c27f716b678f4257df4ff30bcdb945fa88263
João <Sem e-mail>
2026-06-13 11:35:09 -0300

    Inclui validação de no mínimo 3 endereços para uma rota na view

--- src/main/java/com/genetico/views/RotaView.java
+++ src/main/java/com/genetico/views/RotaView.java
@@ -9,10 +9,12 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Route("cadastro-rotas")
@@ -23,6 +25,7 @@ public class RotaView extends VerticalLayout {
     private final MultiSelectComboBox<Endereco> comboBoxEnderecos;
     private final RotaRepository rotaRepository;
     private final List<Endereco> enderecos;
+    private BeanValidationBinder<Rota> rotaBinder;
 
     public RotaView(EnderecoService enderecoService, RotaRepository rotaRepository) {
         this.enderecos = List.copyOf(enderecoService.buscarTodos());
@@ -54,16 +57,32 @@ public class RotaView extends VerticalLayout {
         comboBoxEnderecos.setItemLabelGenerator(Endereco::getLogradouro);
         comboBoxEnderecos.setWidth("100%");
 
+        associarEnderecoBinderAoComboBox(comboBoxEnderecos);
+
         return comboBoxEnderecos;
     }
 
+    private void associarEnderecoBinderAoComboBox(MultiSelectComboBox<Endereco> comboBoxEnderecos) {
+        this.rotaBinder = new BeanValidationBinder<>(Rota.class);
+        this.rotaBinder.forField(comboBoxEnderecos)
+                .withConverter(
+                        ArrayList::new,
+                        HashSet::new
+                )
+                .bind("enderecos");
+    }
+
     private void salvarRota() {
-        var enderecosSelecionadosComboBox = new ArrayList<>(comboBoxEnderecos.getSelectedItems());
+        if (!rotaBinder.validate().hasErrors()) {
+            var enderecosSelecionadosComboBox = new ArrayList<>(comboBoxEnderecos.getSelectedItems());
 
-        var rota = new Rota(enderecosSelecionadosComboBox);
-        rotaRepository.save(rota);
+            var rota = new Rota(enderecosSelecionadosComboBox);
+            rotaRepository.save(rota);
 
-        Notification.show("Rota salva com sucesso", 4500, Notification.Position.MIDDLE);
+            Notification.show("Rota salva com sucesso", 4500, Notification.Position.MIDDLE);
+        } else {
+            Notification.show("Erro ao salvar rota. Verifique erros apresentados na tela", 4500, Notification.Position.MIDDLE);
+        }
     }
 }
 
