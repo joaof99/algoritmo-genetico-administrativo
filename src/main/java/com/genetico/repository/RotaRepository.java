bb20c3c4eefc42327c44b05fa65923c4967350ce
João <Sem e-mail>
2026-08-16 14:29:32 -0300

    8 - Cria RotaRepository

---
+++ src/main/java/com/genetico/repository/RotaRepository.java
@@ -0,0 +1,6 @@
+package com.genetico.repository;
+
+import com.genetico.model.Rota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RotaRepository extends JpaRepository<Rota, Integer> {}
