81bbcbcacfc78830d66172dc798e791a1a12a938
João <Sem e-mail>
2026-08-23 11:50:01 -0300

    8 - Inclui endpoint que retorna endereços de uma rota

--- src/main/java/com/genetico/controller/RotaController.java
+++ src/main/java/com/genetico/controller/RotaController.java
@@ -1,8 +1,11 @@
 package com.genetico.controller;
 
+import com.genetico.model.Endereco;
 import com.genetico.model.Rota;
 import com.genetico.repository.RotaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +25,11 @@ public class RotaController {
         return rotaRepository.findAll();
     }
 
+    @GetMapping("/{id}/enderecos")
+    public ResponseEntity<List<Endereco>> buscarEnderecosPorRotaId(@PathVariable Integer id) {
+        return rotaRepository.findById(id)
+                .map(Rota::getEnderecos)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
