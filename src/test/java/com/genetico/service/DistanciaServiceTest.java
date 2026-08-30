547de95da1bf6a7438b3d84425cb3b474e21cf99
João <Sem e-mail>
2026-08-30 12:18:10 -0300

    8 - Cria teste unitário que valida que api não é chamada desnecessariamente ao buscar vários endereços

--- src/test/java/com/genetico/service/DistanciaServiceTest.java
+++ src/test/java/com/genetico/service/DistanciaServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -51,4 +52,31 @@ class DistanciaServiceTest {
         verify(integracaoLocationIQAPI, never()).buscarDistanciaEntreCoordenadas(any(), any());
     }
 
+    @Test
+    @DisplayName("Não deve buscar na API caso todas as distâncias já possuam suas combinações")
+    void naoDeveBuscarNaAPICasoTodasAsDistanciasJaPossuamSuasCombinacoes() {
+        var endereco = new Endereco();
+        endereco.setId(1);
+
+        var endereco2 = new Endereco();
+        endereco2.setId(2);
+
+        var endereco3 = new Endereco();
+        endereco3.setId(3);
+
+        var distancias = List.of(
+                new Distancia(endereco, endereco2, 3),
+                new Distancia(endereco, endereco3, 3),
+                new Distancia(endereco2, endereco, 3),
+                new Distancia(endereco2, endereco3, 3),
+                new Distancia(endereco3, endereco, 3),
+                new Distancia(endereco3, endereco2, 3)
+        );
+
+        when(distanciaRepository.findAllById(any()))
+                .thenReturn(distancias);
+
+        distanciaService.buscarDistancias(List.of(1, 2, 3));
+        verify(integracaoLocationIQAPI, never()).buscarDistanciaEnderecos(any());
+    }
 }
