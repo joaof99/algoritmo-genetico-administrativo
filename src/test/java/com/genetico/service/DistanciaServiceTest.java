be23a314beb5c6556d499233f49d647c6b2a3a40
João <Sem e-mail>
2026-09-06 12:12:49 -0300

    8 - Melhora teste unitário

--- src/test/java/com/genetico/service/DistanciaServiceTest.java
+++ src/test/java/com/genetico/service/DistanciaServiceTest.java
@@ -3,11 +3,13 @@ package com.genetico.service;
 import com.genetico.api.IntegracaoLocationIQAPI;
 import com.genetico.model.Distancia;
 import com.genetico.model.Endereco;
+import com.genetico.model.Rota;
 import com.genetico.repository.DistanciaRepository;
 import com.genetico.repository.EnderecoRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -75,6 +77,9 @@ class DistanciaServiceTest {
         when(distanciaRepository.findAllById(any()))
                 .thenReturn(distancias);
 
+        when(enderecoRepository.findAllById(any()))
+                .thenReturn(List.of(endereco, endereco2, endereco3));
+
         var distanciaResponses = distanciaService.buscarDistancias(List.of(1, 2, 3));
 
         verify(integracaoLocationIQAPI, never()).buscarDistanciaEnderecos(any());
@@ -103,5 +108,62 @@ class DistanciaServiceTest {
         assertEquals(3, distanciaResponses.get(5).idOrigem());
         assertEquals(2, distanciaResponses.get(5).idDestino());
         assertEquals(100, distanciaResponses.get(5).distancia());
+    }
+
+    @Test
+    @DisplayName("Deve executar corretamente a rotina de busca e armazenagem de distâncias faltantes na API")
+    void deveExecutarCorretamenteARotinaDeBuscaEArmazenagemDeDistanciasFaltantesNaAPI() {
+        var endereco1 = new Endereco();
+        endereco1.setId(1);
+
+        var endereco2 = new Endereco();
+        endereco2.setId(2);
+
+        var endereco3 = new Endereco();
+        endereco3.setId(3);
+
+        var endereco4 = new Endereco();
+        endereco4.setId(4);
+
+        var distancias = List.of(
+                new Distancia(endereco1, endereco2, 50),
+                new Distancia(endereco1, endereco3, 60),
+                new Distancia(endereco1, endereco4, 60),
+                new Distancia(endereco2, endereco1, 70),
+                new Distancia(endereco2, endereco3, 80),
+                new Distancia(endereco2, endereco4, 80),
+                new Distancia(endereco3, endereco1, 80),
+                new Distancia(endereco3, endereco2, 80),
+                new Distancia(endereco3, endereco4, 80),
+                new Distancia(endereco4, endereco1, 80),
+                new Distancia(endereco4, endereco2, 80)
+        );
+
+        when(distanciaRepository.findAllById(any()))
+                .thenReturn(distancias);
+
+        when(enderecoRepository.findAllById(any()))
+                .thenReturn(List.of(endereco1, endereco2, endereco3, endereco4));
+
+        when(integracaoLocationIQAPI.buscarDistanciaEnderecos(any()))
+                .thenReturn(List.of(new Distancia(endereco4, endereco3, 100)));
+
+        distanciaService.buscarDistancias(List.of(1, 2, 3, 4));
+
+        ArgumentCaptor<List<Endereco>> enderecosCaptor = ArgumentCaptor.forClass(List.class);
+        verify(integracaoLocationIQAPI, times(1)).buscarDistanciaEnderecos(enderecosCaptor.capture());
+
+        var enderecosParaBuscaAPI = enderecosCaptor.getValue();
+        assertEquals(2, enderecosParaBuscaAPI.size());
+        assertEquals(3, enderecosParaBuscaAPI.getFirst().getId());
+        assertEquals(4, enderecosParaBuscaAPI.get(1).getId());
+
+        ArgumentCaptor<List<Distancia>> distanciasParaSalvarBanco = ArgumentCaptor.forClass(List.class);
+        verify(distanciaRepository, times(1)).saveAll(distanciasParaSalvarBanco.capture());
+
+        var distanciasSalvasBanco = distanciasParaSalvarBanco.getValue();
+        assertEquals(1, distanciasSalvasBanco.size());
+        assertEquals(4, distanciasSalvasBanco.getFirst().getOrigem().getId());
+        assertEquals(3, distanciasSalvasBanco.getFirst().getDestino().getId());
     }
 }
