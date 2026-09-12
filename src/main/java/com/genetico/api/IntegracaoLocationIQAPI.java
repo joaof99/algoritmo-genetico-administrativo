3067528876fda72a645a163046d84c47925d4d90
João <Sem e-mail>
2026-09-11 22:31:24 -0300

    8 - Remove import não utilizado

--- src/main/java/com/genetico/api/IntegracaoLocationIQAPI.java
+++ src/main/java/com/genetico/api/IntegracaoLocationIQAPI.java
@@ -9,7 +9,6 @@ import com.genetico.model.Endereco;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
